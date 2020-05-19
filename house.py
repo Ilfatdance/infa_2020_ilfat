ac4b3a698cc3dace2f0543bab508dcae6430f54c
Khairullin Ilfat <ilfatdance@mail.ru>
2020-05-19 11:46:01 +0300

    Вызвал функцию рисования домика

--- house.py
+++ house.py
@@ -1,5 +1,15 @@
-from graph.import *
+from graph import *
 
 def main():
+    windowSize(600,500)
+    paint_house(700, 700, 300, 400)
+    run()
+
+def paint_house(x, y, width, height):
+    """ Рисует домик
+        (x, y) - опорная точка, левая-нижняя точка крыши
+    """
     pass
+
+main()
 
